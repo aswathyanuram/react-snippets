3f25d3cd41c6f44e1d16876c43f1e62d863a7b7f
Aswathy <aswathyanuramar@gmail.com>
2024-07-01 13:05:47 +0530

    changes

--- src/18-day/Footer.jsx
+++ src/18-day/Footer.jsx
@@ -4,7 +4,7 @@ import "./businessCard.css";
 export default function () {
   return (
     <div className="footer">
-      <img src="avatars" />
+      <img src="./avatar-image-business-card.png" className="avatar" />
       <div className="name">Amy Burgess</div>
       <div className="details">Customer Manger, Solution Oy</div>
     </div>

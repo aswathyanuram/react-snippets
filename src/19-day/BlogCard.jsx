f63b77e98e58243bcdd191bd7ed881ea4584a5b7
Aswathy <aswathyanuramar@gmail.com>
2024-07-01 15:57:07 +0530

    Blog Card

---
+++ src/19-day/BlogCard.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./blogCard.css";
+
+export default function BlogCard() {
+  return (
+    <div className="container">
+      <div className="blogCard">
+        <div className="header">
+          <div className="image"></div>
+          <button className="btn">Design</button>
+        </div>
+        <div className="content">
+          <div className="title">Embracing Minimalism</div>
+          <div className="details">
+            From minimalist sculptures to minimalist paintings, this blog will
+            inspire you to appreciate the beauty that lies in simplicity.
+          </div>
+        </div>
+
+        <div className="footer">
+          <div className="name"> Annie Spratt</div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import BusinessCard from "./18-day/BusinessCard";
+import BlogCard from "./19-day/BlogCard";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BusinessCard />
+    <BlogCard />
   </React.StrictMode>
 );
